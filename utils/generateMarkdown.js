cc85749efc7e0f8ca70b23c72d436663bbe610c3
Marzena-W <marzena.m.wilk@gmail.com>
2023-02-17 19:39:51 +0000

    added template of ReadMe file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return
+  `
+# ${data.title}
+![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
 
+
+# Description
+> ${data.description}
+
+## Table of Contents
+• [Description](#description)
+• [Installation](#installation)
+• [Usage](#usage)
+• [License](#license)
+• [Contributing](#contributing)
+• [Tests](#tests)
+• [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.licese}
+![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+${data.questions}
 `;
 }
 
